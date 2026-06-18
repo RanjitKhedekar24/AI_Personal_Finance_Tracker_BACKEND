e60c23f1fee1a560f0deb55fa2512dbb1721533f
Ranjit Khedekar <khedekaranjit5@gmail.com>
2026-06-18 16:30:50 +0530

    first commit5

--- server.js
+++ server.js
@@ -30,6 +30,10 @@ app.use(
     credentials: true,
   }),
 );
+app.use(cors({
+  origin: "https://ai-personal-finance-tracker-fronten.vercel.app",
+  credentials: true
+}));
 
 app.options("*", cors());
 app.use(express.json({ limit: "10mb" }));
